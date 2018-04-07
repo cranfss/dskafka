455bc99accd25215690c2d0a13ac5ca7f028815f
Scott Cranford <scott.cranford@ARMYNOISEWET.workdayinternal.com>
2018-04-06 18:03:52 -0700

    remove line to stop package blowup

--- templates/tests/verify-release.sh
+++ templates/tests/verify-release.sh
@@ -6,10 +6,6 @@ set -o pipefail
 set -o xtrace
 
 NAMESPACE="${1:-}"
-if [[ -z "$NAMESPACE" ]];then
-  echo "ERROR: No namespace specified"
-  exit 1
-fi
 
 # Ensure all pods in the namespace entered a Running state
 PODS_FOUND=0
