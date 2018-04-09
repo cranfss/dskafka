224080d4f080e310466b5d995868fcd7e1a7544d
Scott Cranford <scott.cranford@ARMYNOISEWET.workdayinternal.com>
2018-04-09 10:45:36 -0700

    updated dir path for kafkacat

--- scripts/jenkins-backup.sh
+++ scripts/jenkins-backup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+# Copy to /etc/cron.daily and add to crontab -e
+# [ec2-user@ip-172-31-19-73 cron.daily]$ crontab -e
+# 0 0 * * * /etc/cron.daily/jenkins-backup.sh 
 
 date=`date +%F-%H%M%S`
 tar cvfz jenkins-${date}.tgz /var/lib/jenkins
